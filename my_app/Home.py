b87f38b46edc21d8bd68c84eaf0483e95a7ca99e
abhishek <125186953+abhishek887229@users.noreply.github.com>
2024-10-09 07:50:16 +0000

    added basic info of data section

--- my_app/Home.py
+++ my_app/Home.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
-import numpy as np 
+import numpy as np
+import math as mt
+import statistics as sts
 
 def hr():
     st.markdown("<hr>", unsafe_allow_html=True)
@@ -26,9 +28,36 @@ st.write("# basic info of your Data")
 
 count_data=df.count()
 
-tab1, tab2,tab3=st.tabs(["Baisc Info","Cleaning","Feature Engineering"])
+tab1, tab2,tab3=st.tabs(["Over view of your data","Cleaning","Feature Engineering"])
 
 with tab1:
-    st.write(f"your data have {count_data[0]} rows and {len(df.columns)} columns")
 
-print(count_data)
+    col1,col2,col3=st.columns(3)
+
+    with col1:
+        st.subheader("mean")
+        st.write(df['Age'].mean())
+
+    with col2:
+        st.subheader("median")
+        st.write(df['Age'].median())
+
+    with col3:
+        st.subheader("Rows/Columns")
+        st.write(f"{count_data[0]}/{len(df.columns)}")
+        
+    col4,col5,col6=st.columns(3)
+
+    with col4:
+        st.subheader("unique values")
+        st.write(len(df['Age'].unique()))
+
+    with col5:
+        st.subheader("median")
+        st.write(df['Age'].median())
+
+    with col6:
+        st.subheader("mode")
+        st.write(sts.mode(df['Age']))
+        
+    
